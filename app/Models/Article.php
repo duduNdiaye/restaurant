3dff13690c1f9b00c7822109941207090b948ba1
Alioune Badara <badarandao827@gmail.com>
2023-02-15 22:47:08 +0000

    ajout de la relation Many to Many entre commande et article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
     use HasFactory;
     public function user():BelongsTo{
         return $this->belongsTo(User::class);
+    }
+    public function commandes() :BelongsToMany
+    {
+        return $this->belongsToMany(Commande::class);
     }
 }
