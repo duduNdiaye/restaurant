a05d0ee35ebdfadad2b470fa2fef41535c127af1
Alioune Badara <badarandao827@gmail.com>
2023-02-17 22:57:01 +0000

    Dashboard cards et Articles

---
+++ resources/js/Components/DashboardCard.vue
@@ -0,0 +1,62 @@
+<template >
+<div class="grid grid-cols-2 gap-4 sm:grid-cols-4">
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <div class="flex items-center justify-between">
+      <span class="text-gray-500 uppercase font-bold text-sm">Nombre de clients</span>
+      <i class="fas fa-users text-2xl text-gray-500"></i>
+    </div>
+    <div class="mt-4 text-3xl font-semibold">452</div>
+  </div>
+
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <div class="flex items-center justify-between">
+      <span class="text-gray-500 uppercase font-bold text-sm">Nombre de commandes</span>
+      <i class="fas fa-shopping-cart text-2xl text-gray-500"></i>
+    </div>
+    <div class="mt-4 text-3xl font-semibold">876</div>
+  </div>
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <div class="flex items-center justify-between">
+      <span class="text-gray-500 uppercase font-bold text-sm">Nombre d'articles</span>
+      <i class="fas fa-boxes text-2xl text-gray-500"></i>
+    </div>
+    <div class="mt-4 text-3xl font-semibold">2345</div>
+  </div>
+
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <div class="flex items-center justify-between">
+      <span class="text-gray-500 uppercase font-bold text-sm">Revenu total</span>
+      <i class="fas fa-dollar-sign text-2xl text-gray-500"></i>
+    </div>
+    <div class="mt-4 text-3xl font-semibold">$28,765</div>
+  </div>
+</div>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'resources-js-components-dashboard-card',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-zU6KqxUayMHbIW6QIi6Nohj9zHkgCkL27sMWwuxT6ZgKjUk36TdUx2a+iOAGsIbX9ot8iNhHllcOOwZ0g+ZlPw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<style>
+</style>
+
--- resources/js/Pages/Restaurant/Dashboard.vue
+++ resources/js/Pages/Restaurant/Dashboard.vue
@@ -1,10 +1,15 @@
-<script>
+
+
+<script >
+
 export default {
 	  data() {
 	    return {
 	      showDropDown: false,
 	      showSide: true,
-          user: ''
+          user: '',
+          ShowArticles:false,
+          ShowCards:true
 	    }
 	  },
       props: ['user'],
@@ -24,13 +29,25 @@ export default {
 	    toggleDrop() {
 	      this.showDropDown = !this.showDropDown
 
-	    }
+	    },
+        ShowAllArticles(){
+            this.ShowArticles=true
+            this.ShowCards=false
+        },
+        ShowAllCards(){
+            this.ShowArticles=false
+            this.ShowCards=true
+        },
 	  }
 
 	};
 
 </script>
-<script setup></script>
+<script setup>
+
+import DashboardCard from '@/Components/DashboardCard.vue';
+import TableArticles from '@/Components/Tablearticles.vue';
+</script>
 <template>
     <div class="w-screen h-screen flex">
     <!-- Side bar -->
@@ -43,28 +60,24 @@ export default {
       <div class="h-[calc(100vh-50px)] bg-gray-800 py-[20px]">
         <div class="flex flex-col justify-between h-full px-[20px] space-y-[10px]">
           <div class=" flex flex-col justify-between space-y-[10px]">
-            <router-link to="/home" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
-              <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
-              </svg>
-              Home
+            <router-link to="/home" @click="ShowAllCards" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
+
+              Dashboard
             </router-link>
-            <router-link to="/profile" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
-              <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
-              </svg>
-              Profile
+            <router-link to="/profile" @click="ShowAllArticles" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
+
+              Articles
             </router-link>
             <router-link to="/message" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
-              <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z" clip-rule="evenodd"></path>
-              </svg>
-              Messages
+
+              Menus
             </router-link>
             <router-link to="/download" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md rounded-b-lg hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
-              <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z" clip-rule="evenodd"></path>
-              </svg>
+
+              Commandes
+            </router-link>
+            <router-link to="/download" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md rounded-b-lg hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
+
               Download
             </router-link>
 
@@ -124,13 +137,18 @@ export default {
       </div>
       <div class="h-[calc(100vh-50px)] bg-gray-50 p-[20px]">
         <div class="border border-gray-300 rounded-md p-[20px] h-full">
-          <router-view></router-view>
+          <router-view>
+            <DashboardCard v-show="ShowCards"/>
+            <TableArticles v-show="ShowArticles"/>
+          </router-view>
         </div>
       </div>
     </div>
     <!-- Main  -->
 
+
   </div>
+
 </template>
 <style scoped>
 </style>
