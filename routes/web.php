68b1953353702db2e6fbd52baede7f8dd146395d
Alioune Badara <badarandao827@gmail.com>
2023-02-19 18:53:58 +0000

    ROUTES WEBPHP

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,9 @@ Route::middleware([
     })->name('dashboard');
 });
 
+Route::controller(CommandeController::class)->group(function () {
+    Route::get('/commandes', 'client_commande')->name('client.commande');
+});
 Route::post('/article/new',[ArticleController::class,'store']);
 Route::put('article/edit',[ArticleController::class,'update']);
 Route::delete('article/delete', [ArticleController::class,'destroy']);
