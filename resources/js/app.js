c11d15ac5287901e5787a65ab9dcdcde3ba679fa
Alioune Badara <badarandao827@gmail.com>
2023-03-06 02:16:58 +0000

    installation de jquery et de select2

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,11 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import 'select2/dist/css/select2.css';
+import $ from 'jquery' // Importation de jQuery
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
+window.$ = window.jQuery = $ // Déclaration de jQuery comme variable globale
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
